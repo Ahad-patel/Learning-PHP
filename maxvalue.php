0a03e89bb26a9082b91e6f2ed7ff62683975f72e
Ahad-patel <ahadpatel1155@gmail.com>
2020-08-05 13:52:37 -0700

    Task completed

--- maxvalue.php
+++ maxvalue.php
@@ -1,15 +1,38 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>To find max-value</title>
+	<title></title>
 </head>
 <body>
-<p>
-	
-	Hello how are you!
-	What are you doing?
-	How is going there
-	What is your first goal?
-</p>
+	<?php
+  $no = array(12,3,4,11,45);
+ $maximum=maxval($no);
+ echo "The maximum value is =".$maximum;
+  ?>
+
 </body>
 </html>
+
+
+
+<?php 
+
+function maxval($num)
+	{	
+			$max = $num[0];
+			$i = 0;
+			while($i<=4)  
+				{ 
+					if($max < $num[$i])
+					{
+
+						$temp= $num[$i];
+						$num[$i] = $max;
+						$max = $temp;	
+					}
+					$i++;
+				}
+				return $max;
+	}
+
+ ?>
