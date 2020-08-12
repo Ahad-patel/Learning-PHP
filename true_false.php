dbb2eb26a41094d82cc802c318ecc18354b8dc26
Ahad-patel <ahadpatel1155@gmail.com>
2020-08-12 05:12:01 -0700

    file updated

--- true_false.php
+++ true_false.php
@@ -8,11 +8,43 @@
 
 $a = array(0,0.0,"","0",false,null);
 
-foreach ($a as $value) {
-	if ($value == false) {		
-    echo "The value of $value is false";
-	echo "<br>";	}
+echo "The comparison through == is <br>";
+
+foreach ($a as $key => $value) {
 	
+	if ($value == false) {		
+    echo "The key ($key) of $value is false";
+	echo "<br>";
+  }
+	
+}
+
+
+echo "<br> <br>";
+
+echo "The comparison through === is <br>";
+
+foreach ($a as $key2 => $value2) {
+	
+	if ($value2 === false) {		
+    echo "The key ($key2) of $value2 is false";
+	echo "<br>";
+  }
+	
+}
+
+
+echo "<br> <br>";
+
+echo "The comparison directly through array is <br>";
+foreach ($a as $key1 => $value1) {
+	if(array($a))
+{
+	 echo "The key ($key1) of $value1 is false";
+	echo "<br>";
+	
+}
+
 }
 
  ?>
