73769f86e2e51cd7a389b24eb5be6f865f131403
Ahad-patel <ahadpatel1155@gmail.com>
2020-08-12 05:33:02 -0700

    ternaty operator done

---
+++ Ternary_operator.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ternary operator</title>
+</head>
+<body>
+<?php 
+$a = "john";
+$b = "Jim";
+
+echo ($a == "john") ? "My name is $a": "My name is not $a";
+echo "<br><br>";
+ echo ($a == "Jim") ? "My name is $b": "My name is not $b";
+ 
+ ?>
+</body>
+</html>
