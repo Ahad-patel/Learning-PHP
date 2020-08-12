ef659e1106788e3cb0fc9de0b3fca280d951ede8
Ahad-patel <ahadpatel1155@gmail.com>
2020-08-12 06:37:59 -0700

    Not type specific

--- Ternary_operator.php
+++ Ternary_operator.php
@@ -11,6 +11,8 @@ $b = "Jim";
 echo ($a == "john") ? "My name is $a": "My name is not $a";
 echo "<br><br>";
  echo ($a == "Jim") ? "My name is $b": "My name is not $b";
+ echo "<br><br>";
+ echo (5 == "5") ? "It is Not type specific": "It is type specific";
  
  ?>
 </body>
